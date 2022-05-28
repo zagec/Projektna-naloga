114eb752d065052cbecfa1a2b26b95f864da5ca3
Vid Beranic <vidberanic@gmail.com>
2022-05-28 12:57:09 +0200

    header, appjs ...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,39 @@
+import React, {useState, useEffect} from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { UserContext } from "./userContext";
+import Header from './components/Header';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import Home from './components/Home';
+import Registration from './components/Registration';
+
 
 function App() {
+
+  const [user, setUser] = useState(localStorage.user ? JSON.parse(localStorage.user) : null);
+  const updateUserData = (userInfo) => {
+    localStorage.setItem("user", JSON.stringify(userInfo));
+    setUser(userInfo);
+  }
+
   return (
-    <div className="App text-lg font-bold text-center">
-     App
-    </div>
+    <BrowserRouter>
+      <UserContext.Provider value={{
+          user: user,
+          setUserContext: updateUserData
+        }}>
+      < Header/>
+      <Routes>
+           <Route path="/" exact element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Registration />}></Route>
+          <Route path="/logout" element={<Logout />}></Route>
+          {/*<Route path="/profile" element={<Profile />}></Route>
+          <Route path="/photos/:id" element={<ShowPhoto />}></Route> */}
+        </Routes>
+      </UserContext.Provider>
+    </BrowserRouter>
+
   );
 }
 
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { UserContext } from "../userContext";
+import { MdOutlineFastfood } from "react-icons/md";
+import {NavLink} from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div className="bg-lemon p-3 text-xl flex text-slate-200 font-sans">
+    <div className="ml-8 text-2xl font-bold flex">
+        <MdOutlineFastfood className='mr-2' />
+        Restavrantko
+    </div>
+  
+    <div className="ml-auto mr-10">
+      <NavLink className="mr-4" to="/">Domov</NavLink>
+      <UserContext.Consumer>
+        {context => (
+            context.user ?
+              <>
+                <NavLink className='mr-4' to="/profile">Profil</NavLink>
+                <NavLink className='mr-4' to="/logout">Odjavi se</NavLink>
+              </>
+            :
+              <>
+                <NavLink className='mr-4' to='/login'>Prijava</NavLink>
+                <NavLink className='' to='/register'>Registracija</NavLink>
+              </>
+          )}
+        </UserContext.Consumer>
+    </div>
+</div>
+  )
+}
+
+export default Header
