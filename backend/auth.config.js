af422dc9c3b8736fdf283649b82345e829c6aac8
Vid Beranic <vidberanic@gmail.com>
2022-05-30 13:38:33 +0200

    Update auth.config.js

--- backend/auth.config.js
+++ backend/auth.config.js
@@ -1,8 +1,13 @@
 module.exports = {
-    secret: "secret-key",
-    user: "beasty.restaurantFinder@gmail.com", 
-    pass: "beastyZ.U.V", 
-    pass2: "ckhkzclxerkcjyoc",
-    databaseUsername: 'vid',
-    databasePassword: 'database1234'
+  secret: "secret-key",
+  // <<<<<<< vid_frontend
+      user: "beasty.restaurantFinder@gmail.com", 
+      pass: "beastyZ.U.V", 
+  // =======
+      user: "beasty.restaurantFinder@gmail.com",
+      pass: "beastyZ.U.V",
+  // >>>>>>> main
+      pass2: "ckhkzclxerkcjyoc",
+      databaseUsername: 'vid',
+      databasePassword: 'database1234'
   };
